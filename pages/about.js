7130bae98c31cf6143e007c0a2db8940933c6cc5
Ezio <assassinste@gmail.com>
2018-10-04 19:03:38 +0800

    Just the basics

---
+++ pages/about.js
@@ -0,0 +1,5 @@
+export default () => (
+    <div>
+        <p>This is the <strong>about</strong> page!</p>
+    </div>
+)
---
+++ pages/index.js
@@ -0,0 +1,17 @@
+/* 
+    Run by `npm dev`  (With some changes in package.json)
+
+    Wow, the coding procedure is 
+        D Y N A M I C !! 
+    
+    I just press the `Ctrl + S` 
+        Then it's reload, press again, reload again XD
+*/
+
+const Index = () => (
+    <div>
+        <p>Hello <em>Next.js</em></p>
+    </div>
+)
+
+export default Index
