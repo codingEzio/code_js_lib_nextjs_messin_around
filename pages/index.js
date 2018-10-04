5eb62e73169ac2f6ba7bb050e40171436610bef4
Ezio <assassinste@gmail.com>
2018-10-04 23:12:49 +0800

    Known a little more

--- pages/index.js
+++ pages/index.js
@@ -8,8 +8,19 @@
         Then it's reload, press again, reload again XD
 */
 
+import Link from "next/link";
+
+/*
+    Some notes:
+        0.  The 'style' won't work on the outer `Link` (analogy!)
+        1.  You can change `<a>` to `<button>` (ala it supports `onClick`)
+*/
+
 const Index = () => (
     <div>
+        <Link href="/about"> 
+            <button style={{ fontSize: 20}}>About page</button>
+        </Link>
         <p>Hello <em>Next.js</em></p>
     </div>
 )
