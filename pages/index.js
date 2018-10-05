8975f44b111ac82203f9fb40e266dfa982e3067c
Ezio <assassinste@gmail.com>
2018-10-05 17:44:55 +0800

    Fetch search-result, display on homepage

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,7 @@
 
 import Link from "next/link";
 import Layout from '../components/MyLayout'
+import fetch from "isomorphic-unfetch";
 
 /*
     Some notes:
@@ -23,21 +24,30 @@ import Layout from '../components/MyLayout'
     </Link>
 */
 
-const PostLink = (props) => (
-    <li>
-        <Link as={`/p/${props.id}`} href={`/post?title=${props.title}`}>
-            <a>{props.title}</a>
-        </Link>
-    </li>
-)
-
-export default () => (
+const Index = (props) => (
     <Layout>
-        <h1>My Blog</h1>
+        <h1>Marvel's TV Shows</h1>
         <ul>
-            <PostLink id="greeting" title="Hello Next.js" />
-            <PostLink id="praise-yo" title="It's awesome!" />
-            <PostLink id="praise-again" title="And .. It's easy!" />
+            {props.shows.map(({ show }) => (
+                <li key={show.id}>
+                    <Link as={`/p/${show.id}`} href={`/post?id=${show.id}`}>
+                        <a>{show.name}</a>
+                    </Link>
+                </li>
+            ))}
         </ul>
     </Layout>
 )
+
+Index.getInitialProps = async function () {
+    const res = await fetch('https://api.tvmaze.com/search/shows?q=Marvels')
+    const data = await res.json()
+
+    console.log(`Show data fetched. Count: ${data.length}`)
+
+    return {
+        shows: data 
+    }
+}
+
+export default Index
